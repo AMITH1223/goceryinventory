5f82c4a9421ed18fd96dfa13c56ff43e777f4bac
AMITH1223 <amithkashyapsr2324@gmail.com>
2023-10-07 19:06:49 +0530

    amith

--- grocery_inventory_project/Inventory/views.py
+++ grocery_inventory_project/Inventory/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from .models import GroceryItem
 from .Serializers import GroceryItemSerializer
 
+# adding of grocery items
+
 @api_view(['POST'])
 def add_grocery_item(request):
     serializer = GroceryItemSerializer(data=request.data)
@@ -12,6 +14,7 @@ def add_grocery_item(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Retriving of grocery items
 @api_view(['GET'])
 def get_inventory(request):
     items = GroceryItem.objects.all()
